27d151e9ee8440d4cefe3b04c267c10c26821b17
borsema <65385281+borsema@users.noreply.github.com>
2026-06-13 11:37:06 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+
+echo "===== STARTUP.SH STARTED ====="
+
 apt-get update
 apt-get install -y graphviz
+
+echo "===== GRAPHVIZ CHECK ====="
+which dot
+dot -V
+
+echo "===== STARTING STREAMLIT ====="
 
 streamlit run HomeMain.py --server.port 8000 --server.address 0.0.0.0
